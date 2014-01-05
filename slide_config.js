689de45a930e07fb7c0d3003f68d6e847a584156
Alan Cutter <alancutter@chromium.org>
2014-01-05 21:00:25 +0800

    Write up intro and CSS slides

--- slide_config.js
+++ slide_config.js
@@ -1,15 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
+    title: 'Web Animations',
+    subtitle: 'A rich animation model for the web',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/chrome_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -18,20 +17,17 @@ var SLIDE_CONFIG = {
   },
 
   // Author information
-  presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+  presenters: [
+    {name: 'Alan Cutter', company: 'Google Sydney'},
+    {name: 'Eric Willigers', company: 'Google Sydney'},
+    /*{
+      name: 'Third Name',
+      company: 'Job Title, Google',
+      gplus: 'http://plus.google.com/1234567890',
+      twitter: '@yourhandle',
+      www: 'http://www.you.com',
+      github: 'http://github.com/you'
+    }*/
+  ]
 };
 
